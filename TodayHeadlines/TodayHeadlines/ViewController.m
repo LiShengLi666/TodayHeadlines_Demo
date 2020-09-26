7162ea47f4f090908337b07b1193f073d5a09267
13554647573@163.com <xili159753>
2020-09-26 13:54:36 +0800

    闭包添加控件

--- TodayHeadlines/TodayHeadlines/ViewController.m
+++ TodayHeadlines/TodayHeadlines/ViewController.m
@@ -17,7 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+   
 }
 
+/**
+ 闭包方式添加控件
+ */
+- (void)test1{
+    
+    [self.view addSubview:({
+           
+           UILabel *lbl = [[UILabel alloc] init];
+           lbl.text = @"我就是试试";
+           lbl.textColor = [UIColor redColor];
+           // 根据内容变大
+           [lbl sizeToFit];
+           lbl.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+           lbl;
+       })];
+}
 
 @end
